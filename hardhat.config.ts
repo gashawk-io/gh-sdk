6a056970274b973aebe5879f48c813b8a8148741
Alex <alex.plutta@googlemail.com>
2022-08-16 09:14:06 +0200

    clean up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,37 +1,8 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-
 import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-solhint";
-import "@typechain/hardhat";
 import "dotenv/config";
-import "hardhat-deploy";
-import "solidity-coverage";
-
-import "./tasks/accounts";
-import "./tasks/balance";
-import "./tasks/block-number";
-import "./tasks/create-collectibles";
-
-const MAINNET_RPC_URL =
-    process.env.MAINNET_RPC_URL ||
-    process.env.ALCHEMY_MAINNET_RPC_URL ||
-    "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
-    "https://eth-rinkeby.alchemyapi.io/v2/your-api-key";
-const KOVAN_RPC_URL =
-    process.env.KOVAN_RPC_URL ||
-    "https://eth-kovan.alchemyapi.io/v2/your-api-key";
-const MNEMONIC = process.env.MNEMONIC || "your mnemonic";
-const ETHERSCAN_API_KEY =
-    process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
-// optional
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key";
-const PINATA_API_KEY = process.env.PINATA_API_KEY;
-const PINATA_API_SECRET = process.env.PINATA_API_SECRET;
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -43,29 +14,8 @@ module.exports = {
             // }
         },
         localhost: {},
-        kovan: {
-            url: KOVAN_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
-            saveDeployments: true,
-        },
-        rinkeby: {
-            url: RINKEBY_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
-            saveDeployments: true,
-        },
-      
     },
-    etherscan: {
-        // Your API key for Etherscan
-        // Obtain one at https://etherscan.io/
-        apiKey: ETHERSCAN_API_KEY,
-    },
+
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
@@ -75,18 +25,7 @@ module.exports = {
             default: 1,
         },
     },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.4",
-            },
-        ],
-    },
     mocha: {
         timeout: 100000,
-    },
-    typechain: {
-        outDir: "typechain",
-        target: "ethers-v5",
     },
 };
