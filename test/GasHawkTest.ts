28a35e77479b5b5396afd9a1a0bec7ca0376195e
Alex <alex.plutta@googlemail.com>
2022-08-29 16:16:49 +0200

    add test for gashawk constructor

---
+++ test/GasHawkTest.ts
@@ -0,0 +1,25 @@
+import { Gashawk } from "../src/Gashawk";
+import { ethers } from "hardhat";
+import { Signer, Wallet } from "ethers";
+import { expect } from "chai";
+import { NoProviderException } from "../src/Exceptions/NoProviderException";
+
+describe.only("Gashawk Test", () => {
+    let signer: Signer;
+
+    beforeEach(async () => {
+        [signer] = await ethers.getSigners();
+    });
+    it("Create new Gashawk instance when provider is defined", async () => {
+        expect(
+            async () => await Gashawk.fromSigner(signer, "www.example-rpc.com")
+        ).to.not.throw;
+    });
+    it("Throw an exception if the signer has no provider", async () => {
+        const signer = Wallet.createRandom();
+
+        expect(
+            async () => await Gashawk.fromSigner(signer, "www.example-rpc.com")
+        ).to.throw;
+    });
+});
