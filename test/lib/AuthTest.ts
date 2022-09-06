9fc721a9b6dd52ae4d9dd8ff554a94839a915d65
Alex <alex.plutta@googlemail.com>
2022-09-06 12:03:08 +0200

    add tests for Auth

--- test/lib/AuthTest.ts
+++ test/lib/AuthTest.ts
@@ -3,6 +3,7 @@ import chai from "chai";
 import { Signer } from "ethers";
 import { ethers } from "hardhat";
 import LoginFailedException from "../../src/Exceptions/LoginFailedException";
+import { TermsRejectedException } from "../../src/Exceptions/TermsRejectedException";
 import { AuthClient } from "../../src/http/AuthClient";
 import { Auth } from "../../src/lib/Auth";
 
@@ -18,7 +19,7 @@ describe("Auth", () => {
         [signer] = await ethers.getSigners();
     });
     describe("login", () => {
-        it.only("returns token if called login properly", async () => {
+        it("returns token if called login properly and terms are excepted", async () => {
             const expectedToken =
                 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJHYXNIYXdrIiwic3ViIjoiMHhmMzlGZDZlNTFhYWQ4OEY2RjRjZTZhQjg4MjcyNzljZmZGYjkyMjY2IiwiaWF0IjoxNjYwMzI0Nzg1MTkyLCJleHAiOjE2OTE4NjA3ODUzNDgsImp0aSI6IjB4YWM3MDQwMGI3ZWM2OTljZDk1ZGU1NTgzMWJlNWJlN2M0ZGYwNjM2NjI0OTFhNmZhMTU2MDhkNjZiZmEwYmM3OTI4ZDZmODY4MzRmYzAzYTQwZWY2OWFlOWZiN2RkNmE3MDM1MWZhZGE3MjJhMDg1NzUwYWU0ZmUzMWZhNDFkNWExYiIsInJvbGVzIjpbIlVTRVIiXX0.bGqp5-JhBuCmK5bYybjGbeyclsdVz2UgPyRjDtnTdn0";
 
@@ -49,6 +50,25 @@ describe("Auth", () => {
                 expect.fail("method should have thrown");
             } catch (e) {
                 expect(e).to.be.instanceOf(LoginFailedException);
+            }
+        });
+        it("throws terms rejcted exception if user has not accepted terms", async () => {
+            const authClientMock = {
+                login(_: LoginPayload) {
+                    throw new LoginFailedException();
+                },
+            } as unknown as AuthClient;
+
+            try {
+                //Waiting a second until we accept th terms
+                setTimeout(() => {
+                    process.stdin.push("no");
+                    process.stdin.push("\x0D"); //Enter escape char
+                }, 1000);
+                await Auth.login(signer, authClientMock);
+                expect.fail("method should have thrown");
+            } catch (e) {
+                expect(e).to.be.instanceOf(TermsRejectedException);
             }
         });
     });
