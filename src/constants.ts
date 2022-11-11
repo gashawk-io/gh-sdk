40b3f8bcdf1d13286444042082290365790ea14d
Daniel Pfeffer <daniel@corpus.ventures>
2022-11-11 11:49:13 +0100

    Update GH FE URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const GASHAWK_BACKEND_URL =
     process.env.GASHAWK_BACKEND_URL ?? "https://beta-be.gashawk.io:3000";
 export const GASHAWK_FRONTEND_URL =
-    process.env.GASHAWK_FRONTEND_URL ?? "https://dc22.gashawk.io";
+    process.env.GASHAWK_FRONTEND_URL ?? "https://www.gashawk.io";
