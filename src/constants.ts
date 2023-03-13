8ae6174cd41675f85871b7ccd432fe5fad304cb7
Daniel Pfeffer <daniel@corpus.ventures>
2023-03-13 09:05:14 +0100

    Update API URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const GASHAWK_BACKEND_URL =
-    process.env.GASHAWK_BACKEND_URL ?? "https://beta-be.gashawk.io:3000";
+    process.env.GASHAWK_BACKEND_URL ?? "https://core.gashawk.io/api";
 export const GASHAWK_FRONTEND_URL =
     process.env.GASHAWK_FRONTEND_URL ?? "https://www.gashawk.io";
